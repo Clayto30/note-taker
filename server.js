de3e6d979b006f35463faf3a95d70afa5f86faa3
Clayton Goff <cltngoff@gmail.com>
2021-04-17 15:33:46 -0700

    first route, GET, works

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const notes = require('./db/db.json')
+// const apiRoutes = require('./public/assets/js/index')
+app.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
 app.listen(PORT, () => {
     console.log(`Shh, this is a library. Listen on ${PORT}`);
 });
