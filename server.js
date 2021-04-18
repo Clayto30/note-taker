21d27b72a6f5345368e37a2dee7f03cf7d2741d8
Clayton Goff <cltngoff@gmail.com>
2021-04-17 19:26:07 -0700

    html routes written

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 const { notes } = require('./db/db.json')
 const notesArray = notes;
 // const { getNotes, saveNote } = require('./public/assets/js/index')
@@ -39,6 +40,12 @@ app.post('/api/notes', (req, res) => {
     res.json(note);
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
 app.listen(PORT, () => {
     console.log(`Shh, this is a library. Listen on ${PORT}`);
 });
